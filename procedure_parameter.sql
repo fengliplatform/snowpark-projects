4e9e0f463725042eed2c3dc2cde369c6de2218e9
Feng Li <fengliplatform@gmail.com>
2023-02-11 16:35:53 -0500

    Update procedure_parameter.sql

--- procedure_parameter.sql
+++ procedure_parameter.sql
@@ -26,3 +26,51 @@ call example_if(1, 75);
 select * from tmp;
 call example_if(0, 75);
 select * from tmp;
+
+----
+
+
+
+
+//************ SQL Proc Starts ************ 
+
+Create or replace table Emp_Validation (
+
+  Emp_Key number(1),
+
+  Emp_Code varchar(5),
+
+  Emp_Name varchar(10),
+
+  Emp_State varchar(10)   
+
+);
+create or replace procedure copy_file_from_Stage(Target_Table_Name varchar, Stage_Name varchar, Source_File_Name varchar)
+returns varchar not null
+language sql
+Execute as caller
+as
+$$
+declare 
+v_truncate_script varchar:='truncate '||:Target_Table_Name;
+v_copy_into_script varchar:='copy into '||:Target_Table_Name||' from@'||:Stage_Name||'/'||:Source_File_Name||' on_error=continue';
+v_write_errors_script varchar:='create or replace table '||:Target_Table_Name||'_REJECTED As Select * from TABLE(VALIDATE('||:Target_Table_Name||',job_id=>v_copy_into_script_qid))';
+
+begin
+  execute immediate :v_truncate_script;   
+  execute immediate :v_copy_into_script;
+
+  LET v_copy_into_script_qid varchar:='select last_query_id()';
+  execute immediate :v_write_errors_script;
+  return sqlrowcount::varchar;
+exception
+when other then
+return sqlerrm;
+
+end;
+$$;
+//************ SQL Proc Ends ************
+//Calling above proc
+
+call copy_file_from_Stage('AWS.PUBLIC.EMP_VALIDATION','AWS.PUBLIC.EMP_INTERNALSTAGE','Supplier.csv.gz');
+
