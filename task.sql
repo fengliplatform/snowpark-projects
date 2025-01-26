55c96a70e1e9c14e55b5a68d8c0ed55dad2b02f1
fengliplatform <fengliplatform@gmail.com>
2025-01-26 17:01:22 -0500

    change task schedule to 10min

--- task.sql
+++ task.sql
@@ -2,5 +2,5 @@ use role sysadmin;
 use database feng_db;
 create or replace task task1 
     warehouse=feng_wh
-    schedule='5 minutes'
+    schedule='10 minutes'
     as select 'hello from tast1';
